48134d847e65c9d021cbb640dff736722e5f7542
Sudarshan <mail2sudarshan.sharma@gmail.com>
2022-03-28 12:35:09 +0530

    first set of development

---
+++ src/Buttons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+const Buttons = () => {
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+
+  return (
+    <div className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage('decrease')}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button disabled={isLoading} onClick={() => handlePage('increase')}>
+        next
+      </button>
+    </div>
+  );
+};
+
+export default Buttons;
